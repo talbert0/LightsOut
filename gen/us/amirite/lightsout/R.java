2c5ed5aeeb0d008751caceb60a2ab86859c2f2ce
Thomas Ramsey <tramsey1@uoregon.edu>
2011-09-17 00:01:29 -0700

    second commit

--- gen/us/amirite/lightsout/R.java
+++ gen/us/amirite/lightsout/R.java
@@ -19,23 +19,7 @@ public final class R {
         public static final int selector=0x7f020005;
     }
     public static final class id {
-        public static final int but0=0x7f070000;
-        public static final int but1=0x7f070001;
-        public static final int but10=0x7f07000a;
-        public static final int but11=0x7f07000b;
-        public static final int but12=0x7f07000c;
-        public static final int but13=0x7f07000d;
-        public static final int but14=0x7f07000e;
-        public static final int but15=0x7f07000f;
-        public static final int but2=0x7f070002;
-        public static final int but3=0x7f070003;
-        public static final int but4=0x7f070004;
-        public static final int but5=0x7f070005;
-        public static final int but6=0x7f070006;
-        public static final int but7=0x7f070007;
-        public static final int but8=0x7f070008;
-        public static final int but9=0x7f070009;
-        public static final int itemInfo=0x7f070010;
+        public static final int itemInfo=0x7f070000;
     }
     public static final class layout {
         public static final int main=0x7f030000;
--- src/us/amirite/lightsout/LightsOutActivity.java
+++ src/us/amirite/lightsout/LightsOutActivity.java
@@ -8,6 +8,9 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.LinearLayout;
 import android.widget.ToggleButton;
 
 
@@ -15,28 +18,30 @@ import android.widget.ToggleButton;
 
 
 public class LightsOutActivity extends Activity implements OnClickListener {
-    /** Called when the activity is first created. */
+    
+	protected ToggleButton[][] buttonArray;
+	
+	
+	
+	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        findViewById(R.id.but0).setOnClickListener(this);
-        findViewById(R.id.but1).setOnClickListener(this);
-        findViewById(R.id.but2).setOnClickListener(this);
-        findViewById(R.id.but3).setOnClickListener(this);
-        findViewById(R.id.but4).setOnClickListener(this);
-        findViewById(R.id.but5).setOnClickListener(this);
-        findViewById(R.id.but6).setOnClickListener(this);
-        findViewById(R.id.but7).setOnClickListener(this);
-        findViewById(R.id.but8).setOnClickListener(this);
-        findViewById(R.id.but9).setOnClickListener(this);
-        findViewById(R.id.but10).setOnClickListener(this);
-        findViewById(R.id.but11).setOnClickListener(this);
-        findViewById(R.id.but12).setOnClickListener(this);
-        findViewById(R.id.but13).setOnClickListener(this);
-        findViewById(R.id.but14).setOnClickListener(this);
-        findViewById(R.id.but15).setOnClickListener(this);
         
+		buttonArray = new ToggleButton[4][4];
+
+		for (int x = 0; x < 4; x++) {
+			for (int y = 0; y < 4; y++) {
+				buttonArray[x][y] = new ToggleButton(this);
+				LayoutParams params = new LinearLayout.LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT, 1.0f);
+				buttonArray[x][y].setLayoutParams(params);
+				
+				((ViewGroup) findViewById(R.layout.main)).addView(buttonArray[x][y]);
+			}
+		}
+
+
+        setContentView(findViewById(R.layout.main));
     }
     
     
@@ -73,125 +78,13 @@ public class LightsOutActivity extends Activity implements OnClickListener {
 	@Override
 	public void onClick(View v) {
 		gameStatus();
-		ToggleButton b = (ToggleButton) v;
-		switch (b.getId()) {
-		case R.id.but0:
-			((ToggleButton) findViewById(R.id.but1)).toggle();
-			((ToggleButton) findViewById(R.id.but4)).toggle();
-			break;
-			
-		case R.id.but1:
-			((ToggleButton) findViewById(R.id.but0)).toggle();
-			((ToggleButton) findViewById(R.id.but2)).toggle();
-			((ToggleButton) findViewById(R.id.but5)).toggle();
-			break;
 
-		case R.id.but2:
-			((ToggleButton) findViewById(R.id.but1)).toggle();
-			((ToggleButton) findViewById(R.id.but3)).toggle();
-			((ToggleButton) findViewById(R.id.but6)).toggle();
-			break;
-			
-		case R.id.but3:
-			((ToggleButton) findViewById(R.id.but2)).toggle();
-			((ToggleButton) findViewById(R.id.but7)).toggle();
-			break;
-			
-		case R.id.but4:
-			((ToggleButton) findViewById(R.id.but0)).toggle();
-			((ToggleButton) findViewById(R.id.but5)).toggle();
-			((ToggleButton) findViewById(R.id.but8)).toggle();
-			break;
-			
-		case R.id.but5:
-			((ToggleButton) findViewById(R.id.but1)).toggle();
-			((ToggleButton) findViewById(R.id.but4)).toggle();
-			((ToggleButton) findViewById(R.id.but6)).toggle();
-			((ToggleButton) findViewById(R.id.but9)).toggle();
-			break;
-		case R.id.but6:
-			((ToggleButton) findViewById(R.id.but2)).toggle();
-			((ToggleButton) findViewById(R.id.but5)).toggle();
-			((ToggleButton) findViewById(R.id.but7)).toggle();
-			((ToggleButton) findViewById(R.id.but10)).toggle();
-			break;
-			
-		case R.id.but7:
-			((ToggleButton) findViewById(R.id.but3)).toggle();
-			((ToggleButton) findViewById(R.id.but6)).toggle();
-			((ToggleButton) findViewById(R.id.but11)).toggle();
-			break;
-			
-		case R.id.but8:
-			((ToggleButton) findViewById(R.id.but4)).toggle();
-			((ToggleButton) findViewById(R.id.but9)).toggle();
-			((ToggleButton) findViewById(R.id.but12)).toggle();
-			break;
-			
-		case R.id.but9:
-			((ToggleButton) findViewById(R.id.but5)).toggle();
-			((ToggleButton) findViewById(R.id.but8)).toggle();
-			((ToggleButton) findViewById(R.id.but10)).toggle();
-			((ToggleButton) findViewById(R.id.but13)).toggle();
-			break;
-			
-		case R.id.but10:
-			((ToggleButton) findViewById(R.id.but6)).toggle();
-			((ToggleButton) findViewById(R.id.but9)).toggle();
-			((ToggleButton) findViewById(R.id.but11)).toggle();
-			((ToggleButton) findViewById(R.id.but14)).toggle();
-			break;
-			
-		case R.id.but11:
-			((ToggleButton) findViewById(R.id.but7)).toggle();
-			((ToggleButton) findViewById(R.id.but10)).toggle();
-			((ToggleButton) findViewById(R.id.but15)).toggle();
-			break;
-			
-		case R.id.but12:
-			((ToggleButton) findViewById(R.id.but8)).toggle();
-			((ToggleButton) findViewById(R.id.but13)).toggle();
-			break;
-			
-		case R.id.but13:
-			((ToggleButton) findViewById(R.id.but9)).toggle();
-			((ToggleButton) findViewById(R.id.but12)).toggle();
-			((ToggleButton) findViewById(R.id.but14)).toggle();
-			break;
-			
-		case R.id.but14:
-			((ToggleButton) findViewById(R.id.but10)).toggle();
-			((ToggleButton) findViewById(R.id.but13)).toggle();
-			((ToggleButton) findViewById(R.id.but15)).toggle();
-			break;
-			
-		case R.id.but15:
-			((ToggleButton) findViewById(R.id.but11)).toggle();
-			((ToggleButton) findViewById(R.id.but14)).toggle();
-			break;
-		}
 	}
 
 	
 	private void gameStatus() {
 		boolean checker = false;
-	    ToggleButton but0 = (ToggleButton) findViewById(R.id.but0);
-	    ToggleButton but1 = (ToggleButton) findViewById(R.id.but1);
-	    ToggleButton but2 = (ToggleButton) findViewById(R.id.but2);
-	    ToggleButton but3 = (ToggleButton) findViewById(R.id.but3);
-	    ToggleButton but4 = (ToggleButton) findViewById(R.id.but4);
-	    ToggleButton but5 = (ToggleButton) findViewById(R.id.but5);
-	    ToggleButton but6 = (ToggleButton) findViewById(R.id.but6);
-	    ToggleButton but7 = (ToggleButton) findViewById(R.id.but7);
-	    ToggleButton but8 = (ToggleButton) findViewById(R.id.but8);
-	    ToggleButton but9 = (ToggleButton) findViewById(R.id.but9);
-	    ToggleButton but10 = (ToggleButton) findViewById(R.id.but10);
-	    ToggleButton but11 = (ToggleButton) findViewById(R.id.but11);
-	    ToggleButton but12 = (ToggleButton) findViewById(R.id.but12);
-	    ToggleButton but13 = (ToggleButton) findViewById(R.id.but13);
-	    ToggleButton but14 = (ToggleButton) findViewById(R.id.but14);
-	    ToggleButton but15 = (ToggleButton) findViewById(R.id.but15);
-		
+
 	}
 
 	
